76291ae782d3d7049ccf59ec66a833662319c7ac
NajeebArif <NajeebArif786@outlook.com>
2018-06-05 02:16:56 +0530

    Adding the logic to WeatherStation class

--- src/narif/poc/designpatterns/observer/WeatherStation.java
+++ src/narif/poc/designpatterns/observer/WeatherStation.java
@@ -3,6 +3,10 @@
  */
 package narif.poc.designpatterns.observer;
 
+import narif.poc.designpatterns.observer.displayelements.impl.CurrentConditionDisplay;
+import narif.poc.designpatterns.observer.model.WeatherMeasurements;
+import narif.poc.designpatterns.observer.subjects.impl.WeatherData;
+
 /**
  * @author Najeeb
  *
@@ -10,7 +14,10 @@ package narif.poc.designpatterns.observer;
 public class WeatherStation {
 	
 	public static void main(String[] args) {
-		
+		WeatherData weatherData = new WeatherData();
+		CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+		WeatherMeasurements measurements = new WeatherMeasurements(80D, 65D, 30.4D);
+		weatherData.setMeasurements(measurements);
 	}
 
 }
