e3cf6d5c96a5c9d2609b257d5031a05bd8889e60
NajeebArif <NajeebArif786@outlook.com>
2018-06-05 02:13:34 +0530

    Adding the weather station class which will drive this class

---
+++ src/narif/poc/designpatterns/observer/WeatherStation.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package narif.poc.designpatterns.observer;
+
+/**
+ * @author Najeeb
+ *
+ */
+public class WeatherStation {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
